98ce18af3b981a2f7fc70fda030827d10853b808
Shashanka Nataraj <ShashankaNataraj@users.noreply.github.com>
2019-07-27 14:18:21 +0530

    UI almost ready

--- src/Components/Reel/Movie/Thumbnail.jsx
+++ src/Components/Reel/Movie/Thumbnail.jsx
@@ -16,10 +16,21 @@ export default function Thumbnail(props) {
         });
       }}
     >
-      <LazyLoad>
+      <LazyLoad once={true}>
         <img src={props.data.smallImageURL} alt={props.data.title} />
       </LazyLoad>
-      <div className="thumbnail-title">{props.data.title}</div>
+      <div className="thumbnail-container">
+        <div className="thumbnail-title">{props.data.title}</div>
+        <div className="arrow-left" />
+        <div className="stats">
+          <span className="stat">
+            <i className="fa fa-eye" /> {props.data.views}
+          </span>
+          <span className="stat">
+            <i className="fa fa-thumbs-up" /> {props.data.likes}
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
